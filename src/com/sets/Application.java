c0f323ec33215b4217cd4c129d012a9b6ea34344
John Odhiambo <theyohancraft@gmail.com>
2022-09-28 12:39:06 +0300

    First commit in Java

---
+++ src/com/sets/Application.java
@@ -0,0 +1,21 @@
+package com.sets;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
+	
+		NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+		
+		numberTree.subSet(1750, 2750)
+		.stream()
+		.forEach(System.out::println);
+		
+	}
+}
